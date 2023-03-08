268fffcc8594d64837357ec7240662203b50c350
MonsteRico <boilermonster0530@yahoo.com>
2023-03-08 15:01:14 -0500

    beginning home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://matthewgardner.dev",
+	sitemap: true,
+	host: true,
+	integrations: [tailwind(), sitemap(), robotsTxt()],
+});
